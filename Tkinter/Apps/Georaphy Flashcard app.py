21f4eacc9bc4c32e03b8b8fe45ec11dd5ce0c278
parsakhk <parsakhk88@gmail.com>
2021-09-12 17:00:22 +0430

    Geography app

--- Tkinter/Apps/Georaphy Flashcard app.py
+++ Tkinter/Apps/Georaphy Flashcard app.py
@@ -4,15 +4,44 @@ from random import randint
 
 root = Tk()
 root.title("Georaphy flashcard app")
-root.geometry("500x500")
+root.geometry("500x600")
+
+def state_answer():
+    answer = answer_input.get()
+    answer.replace(" ", "")
+
+    if answer == Our_states[rando]:
+        Correct = "Correct! " + Our_states[rando]
+    else:
+        Correct = "Try again! " + Our_states[rando]
+
+    answer_label.config(text=Correct)
 
 def states():
     Hide_all_frames()
     state_frame.pack(fill="both", expand=1)
-    Our_states = ['Texas', 'Berlin', 'Sardinia', 'Tehran~1', 'Ahvaz']
+    global Our_states
+    Our_states = ['Texas', 'Berlin', 'Sardinia', 'Tehran', 'Ahvaz']
+    global rando
     rando = randint(0, len(Our_states)-1)
-    #Create states images
+    rando_state = "./Tkinter/Apps/Assets/Geography Flashcard App/" + Our_states[rando] + ".png"
 
+    #Create states images
+    global state_image
+    state_image = ImageTk.PhotoImage(Image.open(rando_state))
+    show_state = Label(state_frame, image=state_image)
+    show_state.pack(pady=7)
+    global answer_input
+    answer_input = Entry(state_frame, font=("Helvetica", 18))
+    answer_input.pack(pady=15)
+    #Rando Button
+    rando_Button = Button(state_frame, text="Next state", command=states)
+    rando_Button.pack(pady=10)
+    answer_button = Button(state_frame, text="Answer", command=state_answer)
+    answer_button.pack(pady=5)
+    global answer_label
+    answer_label = Label(root, text='')
+    answer_label.pack(pady=5)
 
 def capitals():
     Hide_all_frames()
